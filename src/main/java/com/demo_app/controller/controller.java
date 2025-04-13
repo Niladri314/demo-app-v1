b3f033990d8a2bf76b09b888b6aacad067271d75
nilu <ndebankura01@gmail.com>
2025-04-13 14:05:20 +0530

    second commit

--- src/main/java/com/demo_app/controller/controller.java
+++ src/main/java/com/demo_app/controller/controller.java
@@ -1,11 +1,12 @@
 package com.demo_app.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping
 @RestController
 public class controller {
-    @GetMapping("/message")
+    @GetMapping
     public String getMethod()
     {
         return "Hello World";
